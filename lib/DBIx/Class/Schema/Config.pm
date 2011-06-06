9d0f1ce2117fe1b827175651c62eee37ff2940e3
SymKat <symkat@symkat.com>
2011-06-06 15:14:44 -0700

    Updated POD.

--- lib/DBIx/Class/Schema/Config.pm
+++ lib/DBIx/Class/Schema/Config.pm
@@ -8,7 +8,7 @@ use Data::Dumper;
 sub connection {
     my ( $class, @info ) = @_;
 
-    my $config = $class->_load_credentials( $class->_make_config( @info ) );
+    my $config = $class->load_credentials( $class->_make_config( @info ) );
     
     return $class->SUPER::connection(
         $config->{dsn},
@@ -61,54 +61,59 @@ sub config_paths {
 }
 
 1;
-
 =head1 NAME
 
-DBIx::Class::Schema::Credentials - Manage connection credentials for DBIx::Class::Schema
+DBIx::Class::Schema::Config - Manage connection credentials for DBIx::Class::Schema in a file.
 
 =head1 SYNOPSIS
 
-   /etc/dbic.yaml
-   MY_DATABASE:
-       dsn: "rbi:Pg:host=localhost;database=blog"
-       user: "TheDoctor"
-       password: "dnoPydoleM"
-       options:
-           TraceLevel: 1
+    /etc/dbic.yaml
+    MY_DATABASE:
+        dsn: "rbi:Pg:host=localhost;database=blog"
+        user: "TheDoctor"
+        password: "dnoPydoleM"
+        options:
+            TraceLevel: 1
 
-   package My::Schema
-   use warnings;
-   use strict;
-   
-   use base 'DBIx::Class::Schema::Credentials';
-   __PACKAGE__->load_namespaces;
+    package My::Schema
+    use warnings;
+    use strict;
 
-   package My::Code;
-   use warnings;
-   use strict;
-   use My::Schema;
+    use base 'DBIx::Class::Schema::Config';
+    __PACKAGE__->load_namespaces;
 
-   my $schema = My::Schema->connect('MY_DATABASE');
+    package My::Code;
+    use warnings;
+    use strict;
+    use My::Schema;
+
+    my $schema = My::Schema->connect('MY_DATABASE');
 
 =head1 DESCRIPTION
 
-DBIx::Class::Schema::Credentials is a subclass of DBIx::Class::Schema that allows the loading of credentials from a file.  The actual code itself would only need to know about the name of the database, this aims to make it simpler for operations teams to manage database credentials.
+DBIx::Class::Schema::Config is a subclass of DBIx::Class::Schema 
+that allows the loading of credentials from a file.  The actual code 
+itself would only need to know about the name of the database, this 
+aims to make it simpler for operations teams to manage database credentials.
 
 =head1 CONFIG FILES
 
 This module will load the files in the following order if they exist:
 
-    ./dbic.*
-    ~/.dbic.*
-    /etc/dbic.*
+* ./dbic.*
+* ~/.dbic.*
+* /etc/dbic.*
 
 The files should have an extension that Config::Any recognizes, for example /etc/dbic.B<yaml>.
+
+NOTE:  The first acceptable configuration loaded will be used.  As such, DATABASE in ./dbic.yaml
+would be used before DATABASE in /etc/dbic.yaml.
 
 =head1 OVERRIDING
 
 The API has been designed to be simple to override if you need more specific configuration loading.
 
-=head2 _config_paths
+=head2 config_paths
 
 Override this function to change the configuration paths that are searched, for example:
 
@@ -117,27 +122,34 @@ Override this function to change the configuration paths that are searched, for 
     use strict;
     use base 'DBIx::Class::Schema::Credentials';
     
-   # Override _config_paths to search /var/config/dbic.* and /etc/myproject/project.*
-    sub _config_paths {
+
+    # Override _config_paths to search 
+    # /var/config/dbic.* and /etc/myproject/project.*
+    sub config_paths {
         ( '/var/config/dbic', '/etc/myproject/project' );
     }
 
-=head2 _load_credentials
+=head2 load_credentials
 
-Override this function to change the way that DBIx::Class::Schema::Credentials loads credentials, the functions takes the class name, as well as a hashref.
+Override this function to change the way that DBIx::Class::Schema::Credentials 
+loads credentials, the functions takes the class name, as well as a hashref.
 
-    if called as ->connect( "dbi:Pg:dbname=blog", "user", "password", { TraceLevel => 1 } )
+    Some::Schema->connect( "dbi:Pg:dbname=blog", "user", "password", { TraceLevel => 1 } )
+
+Would result in the following data structure as $config in B<load_credentials($class, $config)>:
 
     {
-        dsn           => "dbi:Pg:dbname=blog",
-        user          => "user",
+        dsn      => "dbi:Pg:dbname=blog",
+        user     => "user",
         password => "password",
-        options     => {
+        options  => {
             TraceLevel => 1,
         },
     }
 
-It if the responsibility of this function to allow passing through of normal ->connect calls, this is done by returning the current configuration is the dsn matches ^dbi:.
+It if the responsibility of this function to allow passing through of normal 
+B<->connect> calls, this is done by returning the current configuration if the 
+dsn matches ^dbi:.
 
 The function should return the same structure.  For instance:
 
@@ -147,15 +159,17 @@ The function should return the same structure.  For instance:
     use base 'DBIx::Class::Schema::Credentials';
     use LWP::Simple;
     use JSON
-    
+
+
     # Load credentials from internal web server.
-    sub _load_credentials {
+    sub load_credentials {
         my ( $class, $config ) = @_;
-        
+
         return $config if $config->{dsn} =~ /^dbi:/i;
 
-       return decode_json( 
-           get( "http://someserver.com/v1.0/database?key=somesecret&db=" . $config->{dsn}  ));
+        return decode_json( 
+            get( "http://someserver.com/v1.0/database?key=somesecret&db=" . 
+                $config->{dsn}  ));
     }
 
 =head1 AUTHOR
@@ -170,7 +184,5 @@ LICENSE file included in this package for more detailed information.
 =head1 AVAILABILITY
 
 The latest version of this software is available through GitHub at
-https://github.com/symkat/DBIx-Class-Schema-Credentials
-
+https://github.com/symkat/DBIx-Class-Schema-Config
 =cut
-
